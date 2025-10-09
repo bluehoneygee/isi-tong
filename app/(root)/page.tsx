ff70670e9d1e69966b0e2ebc4556d955bd263e21
bluehoneygee <bluehoneygee@gmail.com>
2025-10-09 13:28:38 +0700

    Homepage Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,26 +1,28 @@
-import { auth, signOut } from "@/auth";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/route";
-
+import Link from "next/link";
 import React from "react";
 
 const Home = async () => {
-  const session = await auth();
-  console.log(session);
-
   return (
     <>
-      <h1 className="font-bold font-inter text-5xl">Welcome</h1>
-      <h1 className=" font-grotesk text-5xl">Welcome</h1>
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Logout</Button>
-      </form>
+      <section className="w-full flex flex-col-reverse sm:flex-row justify-between gap-4 sm:items-center">
+        <h1 className="text-[30px] font-bold leading-[42px] tracking-tighter">
+          All Questions
+        </h1>
+
+        <Button className="bg-[#EFDD8D] min-h-[46px] px-4 py-3 " asChild>
+          <Link href={ROUTES.ASK_QUESTIONS}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">LOCAL SEARCHBAR</section>
+      HOME FILTER
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+      </div>
     </>
   );
 };
