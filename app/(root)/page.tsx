8808875bf13fc2748b6167f2932f9974fd916995
bluehoneygee <bluehoneygee@gmail.com>
2025-10-10 04:58:22 +0700

    Implement Question Card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
@@ -15,7 +16,12 @@ const questions = [
       { _id: "1", name: "plastik kemasan" },
       { _id: "2", name: "bank sampah" },
     ],
-    author: { _id: "1", name: "Bu Rini" },
+    author: {
+      _id: "1",
+      name: "Bu Rini",
+      image:
+        "https://img.freepik.com/free-vector/smiling-young-man-illustration_1308-174669.jpg",
+    },
     upvotes: 23,
     answers: 7,
     views: 180,
@@ -30,41 +36,16 @@ const questions = [
       { _id: "3", name: "minyak jelantah" },
       { _id: "4", name: "tips rumah tangga" },
     ],
-    author: { _id: "2", name: "Bu Yanti" },
+    author: {
+      _id: "2",
+      name: "Bu Yanti",
+      image:
+        "https://img.freepik.com/free-vector/smiling-young-man-illustration_1308-174669.jpg",
+    },
     upvotes: 35,
     answers: 12,
     views: 250,
     createdAt: new Date("2025-10-05"),
-  },
-  {
-    _id: "3",
-    title: "Cara bikin kompos dari sisa dapur yang gampang gimana?",
-    description:
-      "Aku punya sisa sayur dan kulit buah tiap hari. Pengen coba bikin kompos sendiri, tapi takut bau atau kotor. Ada tipsnya, Bu?",
-    tags: [
-      { _id: "5", name: "kompos" },
-      { _id: "6", name: "organik" },
-    ],
-    author: { _id: "3", name: "Bu Lina" },
-    upvotes: 42,
-    answers: 15,
-    views: 310,
-    createdAt: new Date("2025-10-04"),
-  },
-  {
-    _id: "4",
-    title: "Perlu dicuci nggak sih sebelum buang botol minuman?",
-    description:
-      "Aku kadang males nyuci botol plastik sebelum buang. Tapi katanya harus dicuci dulu biar nggak bau. Gimana pengalaman ibu-ibu lain?",
-    tags: [
-      { _id: "7", name: "plastik" },
-      { _id: "8", name: "daur ulang" },
-    ],
-    author: { _id: "4", name: "Bu Nia" },
-    upvotes: 19,
-    answers: 4,
-    views: 120,
-    createdAt: new Date("2025-10-03"),
   },
 ];
 
@@ -106,7 +87,7 @@ const Home = async ({ searchParams }: SearchParams) => {
       <HomeFilter />
       <div className="mt-10 flex w-full flex-col gap-6">
         {filteredQuestions.map((question) => (
-          <h1 key={question._id}>{question.title}</h1>
+          <QuestionCard key={question._id} question={question} />
         ))}
       </div>
     </>
